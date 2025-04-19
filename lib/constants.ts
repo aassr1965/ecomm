b0e022ec544cb5ecd3c1559706108fd751a527ab
smartbladi <contact@smart-bladi.com>
2025-04-19 20:04:26 +0100

    03 create website layout

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Plomberie Chauffage"
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "La qualité au meilleur prix"
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Site Ecommerce articles de plomberie et chauffage"
