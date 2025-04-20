7dce8d64dac8b1e1c3a2ec53aedf2d03c5e6a5c6
smartbladi <contact@smart-bladi.com>
2025-04-20 13:55:36 +0100

    09-create-product-details-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Plomberie Chauffage"
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "La qualité au meilleur prix"
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Site Ecommerce articles de plomberie et chauffage"
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
