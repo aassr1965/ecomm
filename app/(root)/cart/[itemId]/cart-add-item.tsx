0220b16212e0b936b974d26a42b0b87c0fadeb29
smartbladi <contact@smart-bladi.com>
2025-04-21 06:17:40 +0100

    13-create-cart-sidebar

--- app/(root)/cart/[itemId]/cart-add-item.tsx
+++ app/(root)/cart/[itemId]/cart-add-item.tsx
@@ -69,9 +69,10 @@ export default function CartAddItem({ itemId }: { itemId: string }) {
                 ) : (
                   <div className='flex items-center'>
                     <div>
-                      <span className='text-green-700'>
+                        <span className='text-green-700 italic'>
                         Your order qualifies for FREE Shipping.
-                      </span>{' '}
+                        </span>{' '}
+                        <br/>
                       Choose this option at checkout.
                     </div>
                   </div>
@@ -84,7 +85,7 @@ export default function CartAddItem({ itemId }: { itemId: string }) {
                 </div>
                 <Link
                   href='/checkout'
-                  className={cn(buttonVariants(), 'rounded-full w-full')}
+                  className={cn(buttonVariants(), 'rounded-full w-full cursor-pointer')}
                 >
                   Proceed to checkout (
                   {items.reduce((a, c) => a + c.quantity, 0)} items)
@@ -93,7 +94,7 @@ export default function CartAddItem({ itemId }: { itemId: string }) {
                   href='/cart'
                   className={cn(
                     buttonVariants({ variant: 'outline' }),
-                    'rounded-full w-full'
+                    'rounded-full w-full cursor-pointer'
                   )}
                 >
                   Go to Cart
