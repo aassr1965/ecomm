be47148b7ed7c253994296ac7ab40ddd31941729
smartbladi <contact@smart-bladi.com>
2025-04-21 05:01:08 +0100

    10-create-browsing-history

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,6 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Plomberie Chauffage
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "La qualité au meilleur prix"
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Site Ecommerce articles de plomberie et chauffage"
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
+export const FREE_SHIPPING_MIN_PRICE = Number(
+  process.env.FREE_SHIPPING_MIN_PRICE || 35
+);
